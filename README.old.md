80d5c33eb7f55ccdfbefdb7c5c30440134dc44e0
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-03-14 23:32:54 +0100

    add: create-react-app

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# caba-test-project
---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main">
+      <div className="main__layout"></div>
+    </div>
+  );
+}
+
+export default App;
