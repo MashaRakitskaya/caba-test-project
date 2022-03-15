182cec63c6c6ea3cc4c484ae04f40685893a352e
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-03-15 19:22:27 +0100

    add: in SectionWithPopup PopupWithForm

--- src/components/SectionWithPopup/SectionWithPopup.jsx
+++ src/components/SectionWithPopup/SectionWithPopup.jsx
@@ -1,27 +1,61 @@
-import React from "react";
+import PopupWithForm from "components/PopupWithForm/PopupWithForm";
+import React, { useState } from "react";
 import "./SectionWithPopup.css";
 
 const SectionWithPopup = () => {
+  const [isPopupOpen, setPopupOpen] = useState(false);
+
+  const closePopup = () => {
+    setPopupOpen(false);
+  };
+
+  const closeByOverlay = (event) => {
+    if (event?.target?.classList.contains("popup")) {
+      closePopup();
+    }
+  };
+
+  const handleSubmit = () => {
+    closePopup();
+  };
+
+  const handleButtonClick = () => {
+    setPopupOpen(true);
+  };
+
   return (
-    <section className="section-with-popup">
-      <h1 className="section-with-popup__title">
-        This is your test task good luck
-      </h1>
-      <p className="section-with-popup__subtitle">
-        BoomTelecom crypto investments made simple
-      </p>
-      <div className="section-with-popup__box-button-link">
-        <button className="section-with-popup__button">Open popup</button>
-        <a
-          target={"_blank"}
-          className="section-with-popup__link"
-          href="https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna"
-          rel="noreferrer"
-        >
-          some link
-        </a>
-      </div>
-    </section>
+    <>
+      <section className="section-with-popup">
+        <h1 className="section-with-popup__title">
+          This is your test task good luck
+        </h1>
+        <p className="section-with-popup__subtitle">
+          BoomTelecom crypto investments made simple
+        </p>
+        <div className="section-with-popup__box-button-link">
+          <button
+            onClick={() => handleButtonClick()}
+            className="section-with-popup__button"
+          >
+            Open popup
+          </button>
+          <a
+            target={"_blank"}
+            className="section-with-popup__link"
+            href="https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna"
+            rel="noreferrer"
+          >
+            some link
+          </a>
+        </div>
+      </section>
+      <PopupWithForm
+        isOpen={isPopupOpen}
+        onClose={closePopup}
+        onOvarlayClose={closeByOverlay}
+        onSubmit={handleSubmit}
+      />
+    </>
   );
 };
 
